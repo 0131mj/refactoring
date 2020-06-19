9ef52502758dcdabde25a2fae445289de3e3ffa1
mjbae <0131mj@gmail.com>
2020-06-20 02:18:04 +0900

    - 변수 선언(초기화)을 반복문 앞으로 이동

--- 01_Play/statement.js
+++ 01_Play/statement.js
@@ -7,7 +7,7 @@ const invoice = JSON.parse(fs.readFileSync('invoices.json'))[0];
 
 function statement(invoice, plays) {
     let totalAmount = 0;
-    let volumeCredits = 0;
+
 
     let result = `청구 내역 (고객명: ${invoice.customer})\n`;
 
@@ -18,6 +18,7 @@ function statement(invoice, plays) {
         totalAmount += amountFor(perf);
     }
 
+    let volumeCredits = 0;
     for (let perf of invoice.performances) {
         volumeCredits += volumeCreditsFor(perf);
     }
