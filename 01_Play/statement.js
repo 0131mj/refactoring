c3a3aa58a6745c4db6c2e1864d31af619fad8098
mjbae <0131mj@gmail.com>
2020-06-20 07:16:41 +0900

    - volumeCredits 구하는 코드 : 루프를 포함하여 하나의 함수로 추출

--- 01_Play/statement.js
+++ 01_Play/statement.js
@@ -6,26 +6,27 @@ const plays = JSON.parse(fs.readFileSync('plays.json'));
 const invoice = JSON.parse(fs.readFileSync('invoices.json'))[0];
 
 function statement(invoice, plays) {
+
     let totalAmount = 0;
-
-
     let result = `청구 내역 (고객명: ${invoice.customer})\n`;
-
     for (let perf of invoice.performances) {
-
         // 청구내역을 출력한다.
         result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
         totalAmount += amountFor(perf);
     }
 
-    let volumeCredits = 0;
-    for (let perf of invoice.performances) {
-        volumeCredits += volumeCreditsFor(perf);
-    }
-
+    let volumeCredits = totalVolumeCredits();
     result += `총액 : ${usd(totalAmount)}\n`;
     result += `적립 포인트 : ${volumeCredits}점\n`;
     return result;
+
+    function totalVolumeCredits() {
+        let volumeCredits = 0;
+        for (let perf of invoice.performances) {
+            volumeCredits += volumeCreditsFor(perf);
+        }
+        return volumeCredits;
+    }
 
     function usd(aNumber) {
         return new Intl.NumberFormat("en-US", {
