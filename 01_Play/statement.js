6c98ef5dcaa753f39f52bbb7020169e1fb190bc0
mjbae <0131mj@gmail.com>
2020-06-20 23:37:50 +0900

    - totalAmount 루프문에서 분리, appleSauce 라는 임의의 이름으로 함수화 추출

--- 01_Play/statement.js
+++ 01_Play/statement.js
@@ -7,17 +7,23 @@ const invoice = JSON.parse(fs.readFileSync('invoices.json'))[0];
 
 function statement(invoice, plays) {
 
-    let totalAmount = 0;
+    let totalAmount = appleSauce();
     let result = `청구 내역 (고객명: ${invoice.customer})\n`;
     for (let perf of invoice.performances) {
-        // 청구내역을 출력한다.
         result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
-        totalAmount += amountFor(perf);
     }
 
     result += `총액 : ${usd(totalAmount)}\n`;
     result += `적립 포인트 : ${totalVolumeCredits()}점\n`;
     return result;
+
+    function appleSauce() {
+        let totalAmount = 0;
+        for (let perf of invoice.performances) {
+            totalAmount += amountFor(perf);
+        }
+        return totalAmount;
+    }
 
     function totalVolumeCredits() {
         let volumeCredits = 0;
