41beb49d7f06effa7f2cba86051d8d0ae8f3bb0e
mjbae <0131mj@gmail.com>
2020-06-24 01:14:14 +0900

    - result html 로 만들어 렌더링

--- 01_Play/statement.js
+++ 01_Play/statement.js
@@ -6,13 +6,24 @@ function statement(invoice, plays) {
 }
 
 function renderPlainText(data) {
-    let result = `청구 내역 (고객명: ${data.customer})\n`;
+    let result = `<h1>청구 내역 (고객명: ${data.customer})</h1>`;
+    result += `<table>`;
+    result += `<tr>
+                    <th>연극</th>
+                    <th>좌석수</th>
+                    <th>금액</th>
+               </tr>`;
     for (let perf of data.performances) {
-        result += `  ${perf.play.name}: ${usd(perf.amount)} (${perf.audience}석)\n`;
+        result += `  
+                <tr>
+                    <td>${perf.play.name}</td>
+                    <td>(${perf.audience}석)</td>
+                    <td>${usd(perf.amount)}</td>
+                </tr>`;
     }
-
-    result += `총액 : ${usd(data.totalAmount)}\n`;
-    result += `적립 포인트 : ${data.totalVolumeCredits}점\n`;
+    result += `</table>\n`;
+    result += `<p>총액 : <em>${usd(data.totalAmount)}</em></p>`;
+    result += `<p>적립 포인트 : <em>${data.totalVolumeCredits}</em>점</p>`;
     return result;
 
     function usd(aNumber) {
@@ -32,7 +43,7 @@ Promise.all([
 }).then((data) => {
     const {plays, invoices} = data;
     const result = statement(invoices[0], plays);
-    console.log(result);
+    document.body.innerHTML = result;
 }).catch((err) => {
     console.error(err);
 });
