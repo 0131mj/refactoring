582fc2367289e5696c9dd1de511b10aab3531b0e
mjbae <0131mj@gmail.com>
2020-06-24 23:22:43 +0900

    - result 렌더링 분리 : html, plainText

--- 01_Play/statement.js
+++ 01_Play/statement.js
@@ -6,6 +6,27 @@ function statement(invoice, plays) {
 }
 
 function renderPlainText(data) {
+    let result = `청구 내역 (고객명: ${data.customer})\n`;
+    for (let perf of data.performances) {
+        result += `  ${perf.play.name}: ${usd(perf.amount)} (${perf.audience}석)\n`;
+    }
+
+    result += `총액 : ${usd(data.totalAmount)}\n`;
+    result += `적립 포인트 : ${data.totalVolumeCredits}점\n`;
+    return result;
+
+    function usd(aNumber) {
+        return new Intl.NumberFormat("en-US", {
+            style: "currency", currency: "USD", minimumFractionDigits: 2
+        }).format(aNumber / 100);
+    }
+}
+
+function htmlStatement(invoice, plays) {
+    return renderHtml(createStatementData(invoice, plays));
+}
+
+function renderHtml(data) {
     let result = `<h1>청구 내역 (고객명: ${data.customer})</h1>`;
     result += `<table>`;
     result += `<tr>
@@ -21,7 +42,7 @@ function renderPlainText(data) {
                     <td>${usd(perf.amount)}</td>
                 </tr>`;
     }
-    result += `</table>\n`;
+    result += `</table>`;
     result += `<p>총액 : <em>${usd(data.totalAmount)}</em></p>`;
     result += `<p>적립 포인트 : <em>${data.totalVolumeCredits}</em>점</p>`;
     return result;
@@ -42,8 +63,11 @@ Promise.all([
     return {plays, invoices};
 }).then((data) => {
     const {plays, invoices} = data;
-    const result = statement(invoices[0], plays);
-    document.body.innerHTML = result;
+    const plainResult = statement(invoices[0], plays);
+    console.log(plainResult);
+    const htmlResult = htmlStatement(invoices[0], plays);
+    document.body.innerHTML = htmlResult;
+
 }).catch((err) => {
     console.error(err);
 });
